1a96026dc20ead96b51932b15ebafdc519e1450c
Abreto Fu <m@abreto.net>
2017-06-21 12:06:40 +0800

    fix a bug

--- include/correction.hxx
+++ include/correction.hxx
@@ -6,6 +6,6 @@
 
 using namespace cv;
 
-void correction_pitch(Point2f src, Point2f& dst, double pitch_angle_deg)
+void correction_pitch(Point2f src, Point2f& dst, double pitch_angle_deg);
 
 #endif
