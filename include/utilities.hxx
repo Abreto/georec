aea1628956d04fc2f57ea551e651f179c2d7a40f
Abreto Fu <m@abreto.net>
2017-06-21 12:06:10 +0800

    use fabs in std

--- include/utilities.hxx
+++ include/utilities.hxx
@@ -2,6 +2,10 @@
 #ifndef LIBGEOREC_UTILITIES_H
 #define LIBGEOREC_UTILITIES_H
 
+#include <cmath>
+
+using namespace std;
+
 #define EPSILON (1e-8)
 
 #endif
--- lib/utilities.cxx
+++ lib/utilities.cxx
@@ -1,19 +1,10 @@
 /*  */
 #include <utilities.hxx>
 
-double gabsf(double x)
-{
-    if( x > EPSILON )
-        return x;
-    else if ( x < -EPSILON )
-        return -x;
-    else
-        return 0.0;
-}
 
 int equalf(double a, double b)
 {
-    if( gabsf(a-b) > 0.0 )
+    if( fabs(a-b) > EPSILON )
         return 0;
     else
         return 1;
