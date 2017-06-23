7cb14c491b03564c6dbc1f4076a3c8c84cd32300
Abreto Fu <m@abreto.net>
2017-06-23 18:42:40 +0800

    fix error head

--- include/correction.hxx
+++ include/correction.hxx
@@ -3,9 +3,6 @@
 #define LIBGEOREC_CORRECTION_HXX
 
 #include <utilities.hxx>
-#include <opencv2/core.hpp>
-
-using namespace cv;
 
 void correction_pitch(camera_t cam, Point2f src, Point2f& dst, double pitch_angle_deg);
 
--- include/utilities.hxx
+++ include/utilities.hxx
@@ -3,11 +3,17 @@
 #define LIBGEOREC_UTILITIES_H
 
 #include <cmath>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 #define EPSILON (1e-8)
 #define PI      (acos(-1.0))
+
+typedef vector<Point2f> vecp2f;
 
 struct camera_t
 {
