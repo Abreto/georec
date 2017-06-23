dcc22c2f0dd8c7ffe8b7d34a3559be836a1390f7
Abreto Fu <m@abreto.net>
2017-06-23 22:19:33 +0800

    correction ok

--- include/correction.hxx
+++ include/correction.hxx
@@ -5,6 +5,9 @@
 #include <utilities.hxx>
 
 void correction_pitch(camera_t cam, Point2f src, Point2f& dst, double pitch_angle_deg);
-void correction_pitch(camera_t cam, vecp2f src, vecp2f dst, double pitch_angle_deg)
+void correction_pitch(camera_t cam, vecp2f src, vecp2f& dst, double pitch_angle_deg);
+
+void correction_roll(camera_t cam, Point2f src, Point2f& dst, double roll_angle_deg);
+void correction_roll(camera_t cam, vecp2f src, vecp2f& dst, double roll_angle_deg);
 
 #endif
--- lib/correction.cxx
+++ lib/correction.cxx
@@ -4,7 +4,7 @@
 
 void correction_pitch(camera_t cam, Point2f src, Point2f& dst, double pitch_angle_deg)
 {
-    if( equalf(pitch_angle_deg, 0) )
+    if( equalf(pitch_angle_deg, 0.0) )
     {
         dst.x = src.x;
         dst.y = src.y; /* in case copy by ref */
@@ -27,4 +27,31 @@ void correction_pitch(camera_t cam, vecp2f src, vecp2f& dst, double pitch_angle_
     int i = 0, n = src.size();
     for(i = 0;i < n;++i)
         correction_pitch(cam, src[i], dst[i], pitch_angle_deg);
+}
+
+void correction_roll(camera_t cam, Point2f src, Point2f& dst, double roll_angle_deg)
+{
+    if( equalf(roll_angle_deg, 0.0) )
+    {
+        dst.x = src.x;
+        dst.y = src.y;
+    } else {
+        double roll_angle_rad = (roll_angle_deg / 180.0) * PI;
+        double roll_angle_sec = 1.0 / cos(roll_angle_rad);
+        double dx = src.x - cam.x, dy = src.y - cam.y;
+        double L = dx / cam.focus;
+        double spread_angle = atan(L);
+        double ky = cos(spread_angle) / cos(spread_angle - roll_angle_rad);
+        double kx = roll_angle_sec * ky;
+
+        dst.x = kx * dx + cam.x;
+        dst.y = ky * dy + cam.y;
+    }
+}
+
+void correction_roll(camera_t cam, vecp2f src, vecp2f& dst, double roll_angle_deg)
+{
+    int i = 0, n = src.size();
+    for(i = 0;i < n;++i)
+        correction_roll(cam, src[i], dst[i], roll_angle_deg);
 }
