615d6b353333f93b452a4b40ab68c26ef0e26223
Abreto Fu <m@abreto.net>
2017-06-24 20:26:01 +0800

    transform maybe ok

--- lib/transformation.cxx
+++ lib/transformation.cxx
@@ -4,5 +4,10 @@
 
 void projection_trans(Mat src, Mat& dst, vecp2f original, vecp2f corrected, int interp)
 {
-    ;
+    Mat transm = getPerspectiveTransform(original, corrected);
+
+    /* TO BE TESTED */
+    warpPerspective(src, dst, transm, src.size());
+
+    /* TODO: resample */
 }
