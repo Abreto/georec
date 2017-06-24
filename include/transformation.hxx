9d00057dd75870e114f45f7067f2d8a9a97d4df3
Abreto Fu <m@abreto.net>
2017-06-24 19:22:37 +0800

    protrans def

--- include/transformation.hxx
+++ include/transformation.hxx
@@ -3,6 +3,8 @@
 #ifndef LIBGEOREC_TRANSFORMATION_HXX
 #define LIBGEOREC_TRANSFORMATION_HXX
 
+#include <opencv2/opencv.hpp>
 
+void projection_trans(Mat src, Mat& dst, vecp2f original, vecp2f corrected, int interp);
 
 #endif
---
+++ lib/transformation.cxx
@@ -0,0 +1,8 @@
+/*  */
+#include <utilities.hxx>
+#include <transformation.hxx>
+
+void projection_trans(Mat src, Mat& dst, vecp2f original, vecp2f corrected, int interp)
+{
+    ;
+}
