89abd17e6a80c0393ad107d3dc0e46ebcd4e887c
Abreto Fu <m@abreto.net>
2017-06-24 20:36:26 +0800

    fix bugs

--- include/transformation.hxx
+++ include/transformation.hxx
@@ -3,9 +3,7 @@
 #ifndef LIBGEOREC_TRANSFORMATION_HXX
 #define LIBGEOREC_TRANSFORMATION_HXX
 
-#include <opencv2/opencv.hpp>
-
-using namespace cv;
+#include <utilities.hxx>
 
 void projection_trans(Mat src, Mat& dst, vecp2f original, vecp2f corrected, int interp);
 
--- lib/transformation.cxx
+++ lib/transformation.cxx
@@ -1,5 +1,4 @@
 /*  */
-#include <utilities.hxx>
 #include <transformation.hxx>
 
 void projection_trans(Mat src, Mat& dst, vecp2f original, vecp2f corrected, int interp)
