c9a1e9896e52277dc21ce6d6717cc1b79a327eb3
Abreto Fu <m@abreto.net>
2017-06-24 21:13:40 +0800

    fix bugs

--- include/rectification.hxx
+++ include/rectification.hxx
@@ -3,6 +3,7 @@
 #define LIBGEOREC_RECTIFICATION_HXX
 
 #include <utilities.hxx>
+#include <correction.hxx>
 
 void image_rectification(Mat src, Mat& dst, camera_t cam, double pitch_angle_deg, double roll_angle_deg, int order);
 
--- src/georec.cxx
+++ src/georec.cxx
@@ -1,5 +1,12 @@
+/*  */
+#include <rectification.hxx>
 
 int main(void)
 {
+    Mat img = imread("tst.jpg");
+    Mat proccessed;
+    camera_t cam = {3805.6398, 1701.2570, 1142.9699};
+    image_rectification(img, proccessed, cam, 1.2, 1017.466, FIRST_PITCH | THEN_ROLL);
+    imwrite("tsted.jpg", proccessed);
     return 0;
 }
